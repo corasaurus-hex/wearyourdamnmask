32b555c250c11f2751ec6679f5120c3ae67d1348
Cora Sutton <cora@sutton.me>
2020-08-04 15:07:39 -0500

    Add mass shooting deaths

--- build.rb
+++ build.rb
@@ -13,7 +13,17 @@ summary = JSON.parse(URI.open("https://api.covid19api.com/summary").read)["Count
 covid_deaths = summary["TotalDeaths"]
 as_of = DateTime.parse(summary["Date"]).to_time.rfc822.gsub("+0000", "UTC")
 
+
 class Context < Struct.new(:template, :css, :covid_deaths, :as_of)
+  MASS_SHOOTING_DEATHS = {
+    "2019" => 465,
+    "2018" => 371,
+    "2017" => 437,
+    "2016" => 451,
+    "2015" => 368,
+    "2014" => 262
+  }
+
   EVENT_DEATHS = {
     "WW2" => 405_399,
     "WW1" => 116_516,
@@ -23,14 +33,16 @@ class Context < Struct.new(:template, :css, :covid_deaths, :as_of)
     "Revolutionary War" => 25_000,
     "Iraq War" => 4_576,
     "9/11" => 2_977,
+    "2014-19 Mass Shootings" => MASS_SHOOTING_DEATHS.values.sum,
     "Afghanistan War" => 2_216
-  }
+  }.sort_by{|_, deaths| -deaths }
 
   SOURCES = {
     "Wikipedia - US War Casualties" => "https://en.wikipedia.org/wiki/United_States_military_casualties_of_war#Wars_ranked_by_total_number_of_U.S._military_deaths",
     "Wikipedia - 9/11 Casualties" => "https://en.wikipedia.org/wiki/Casualties_of_the_September_11_attacks",
     "CDC - 2018-2019 Flu Deaths" => "https://www.cdc.gov/flu/about/burden/2018-2019.html",
     "COVID 19 API" => "https://covid19api.com/",
+    "Gun Violence Archive" => "https://www.gunviolencearchive.org/",
   }
 
   def number_with_delimiter(number)
