0f983be109324285958cacc5d2dad7ac794c1363
Nate Sutton <nate@sutton.me>
2021-01-23 23:21:12 -0600

    Add city that deaths exceed

--- build.rb
+++ build.rb
@@ -6,15 +6,16 @@ require "erb"
 require "time"
 require "fileutils"
 
-template = File.open("index.erb", "rb", encoding: "utf-8", &:read)
+template = File.open("index.html.erb", "rb", encoding: "utf-8", &:read)
 css = File.open("main.css", "rb", encoding: "utf-8", &:read)
 
 summary = JSON.parse(URI.open("https://api.covid19api.com/summary").read)["Countries"].find{|c| c["CountryCode"] == "US" }
 covid_deaths = summary["TotalDeaths"]
 as_of = DateTime.parse(summary["Date"]).to_time.rfc822.gsub("+0000", "UTC")
 
+cities = JSON.parse(File.read("cities.json"), symbolize_names: true)[:cities]
 
-class Context < Struct.new(:template, :css, :covid_deaths, :as_of)
+class Context < Struct.new(:template, :css, :covid_deaths, :as_of, :cities)
   MASS_SHOOTING_DEATHS = {
     "2019" => 465,
     "2018" => 371,
@@ -42,11 +43,20 @@ class Context < Struct.new(:template, :css, :covid_deaths, :as_of)
   SOURCES = {
     "Wikipedia - US War Casualties" => "https://en.wikipedia.org/wiki/United_States_military_casualties_of_war#Wars_ranked_by_total_number_of_U.S._military_deaths",
     "Wikipedia - 9/11 Casualties" => "https://en.wikipedia.org/wiki/Casualties_of_the_September_11_attacks",
+    "Wikipedia - List of US Cities by Population" => "https://en.wikipedia.org/wiki/List_of_United_States_cities_by_population",
     "CDC - 2018-2019 Flu Deaths" => "https://www.cdc.gov/flu/about/burden/2018-2019.html",
     "CDC - 1918 Pandemic" => "https://www.cdc.gov/flu/pandemic-resources/1918-pandemic-h1n1.html",
     "COVID 19 API" => "https://covid19api.com/",
     "Gun Violence Archive" => "https://www.gunviolencearchive.org/",
   }
+
+  def city_beneath(count=covid_deaths)
+    cities.select{|c| c[:population] <= count }.sort_by{|c| c[:population] }.last
+  end
+
+  def city_above(count=covid_deaths)
+    cities.select{|c| c[:population] >= count }.sort_by{|c| c[:population] }.first
+  end
 
   def number_with_delimiter(number)
     integral, fractional = number.to_s.split(".")
@@ -77,5 +87,5 @@ end
 
 FileUtils.mkdir_p("build")
 FileUtils.cp("robots.txt", "build/robots.txt")
-File.write("build/index.html", Context.new(template, css, covid_deaths, as_of).render)
+File.write("build/index.html", Context.new(template, css, covid_deaths, as_of, cities).render)
 
